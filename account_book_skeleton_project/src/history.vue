d2cd7b71dd1525b2833108969acd05451caa7c6b
eden <feijoaplz@gmail.com>
2024-06-11 12:28:52 +0900

    feat: history data 추가

---
+++ account_book_skeleton_project/src/history.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="historyList">
+    <h2>History</h2>
+  </div>
+</template>
+
+<script setup>
+data();
+return {
+  HistoryList: [
+    {
+      id: 1,
+      accountName: "",
+      date: "요일",
+      purchaseMethod: "구매수단",
+      category: "",
+      amount: "",
+      memo: "",
+    },
+    {
+      id: 2,
+      accountName: "",
+      date: "요일",
+      purchaseMethod: "구매수단",
+      category: "",
+      amount: "",
+      memo: "",
+    },
+    {
+      id: 3,
+      accountName: "",
+      date: "요일",
+      purchaseMethod: "구매수단",
+      category: "",
+      amount: "",
+      memo: "",
+    },
+    {
+      id: 4,
+      accountName: "",
+      date: "요일",
+      purchaseMethod: "구매수단",
+      category: "",
+      amount: "",
+      memo: "",
+    },
+    {
+      id: 5,
+      accountName: "",
+      date: "요일",
+      purchaseMethod: "구매수단",
+      category: "",
+      amount: "",
+      memo: "",
+    },
+  ],
+};
+</script>
+
+<style lang="scss" scoped></style>
