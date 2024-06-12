7d8a55cb607ef8ad44a971364b60fb969c98c594
eden <feijoaplz@gmail.com>
2024-06-12 09:51:18 +0900

    feat: 차트에서 상위 순위 4개를 나타내는 틀

--- account_book_skeleton_project/src/pages/Summary.vue
+++ account_book_skeleton_project/src/pages/Summary.vue
@@ -6,12 +6,17 @@
       <span>지출: {{ budget.monthExpense }}</span>
       <span>순수익: {{ budget.netIncome }}</span>
     </div>
+    <div class="topContainer"></div>
+    <div v-for="(History, index) in topItems" :key="index" class="HistoryList">
+        <h4>{{ History.HistoryList }}</h4>
   </div>
 </template>
 <script setup>
 import { useRoute } from "vue-router";
 import { reactive } from "vue";
 import { useHistoryStore } from "@/stores/historyList.js";
+import {} from "vue-char.js";
+
 const router = useRouter();
 const { createHistory } = useHistoryListStore();
 
