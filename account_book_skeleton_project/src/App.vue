43eb531a06e942d6a4d876a70da18f5f8345ba9c
Oh Ji Hyeon <dhwlgus0419@gmail.com>
2024-06-13 16:59:19 +0900

    historylist 기본작성

--- account_book_skeleton_project/src/App.vue
+++ account_book_skeleton_project/src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
+  <div id="app">
+    {{ message }}
     <h1>Account Book Project!</h1>
   </div>
 </template>
 <script>
 export default {
   name: 'app',
+  data: {
+    message: 'Helloworld!',
+  },
 };
 </script>
 <style></style>
